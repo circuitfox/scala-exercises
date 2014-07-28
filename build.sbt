0ffe2e5dac2ac98fe7660c46bf520754454caa10
circuitfox <circuitfox@hotmail.com>
2014-07-28 15:37:17 -0500

    Structure and build cleanup

--- build.sbt
+++ build.sbt
@@ -4,6 +4,14 @@ version := "0.0.1"
 
 scalaVersion := "2.11.2"
 
+val scalacOpts = Seq("")
+
+val scaladocOpts = Seq("-diagrams")
+
 val deps = Seq("org.scalatest" % "scalatest_2.11" % "2.2.0" % "test")
 
 libraryDependencies ++= deps
+
+scalacOptions ++= scalacOpts
+
+scalacOptions in (Compile, doc) ++= scaladocOpts
--- src/main/scala/com/github/circuitfox/exercises/BeginnerExercises.scala
+++ src/main/scala/com/github/circuitfox/exercises/BeginnerExercises.scala
@@ -1,8 +1,28 @@
-// Copyright (c) 2014 Chris Stankus under the MIT license
-package io.github.circuitfox.scala.exercises
+/*
+ * Copyright (c) 2014 Chris Stankus
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.github.circuitfox.exercises
 
 import scala.annotation.tailrec
-import scala.collection.mutable
 
 /**
  * Solutions for 'Scala exercises for Beginners'.
@@ -78,5 +98,5 @@ object BeginnerExercises extends App {
     "reverse" -> (reverse(tList) == tList.reverse)
   )
 
-  println(testMap forall { case (_, _) => true})
+  println(testMap forall {case (_, _) => true})
 }
